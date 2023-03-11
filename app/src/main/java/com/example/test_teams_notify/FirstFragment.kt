27e62cb71827dfd1384ecc64cf75cc5a65a33d5f
VTN <nghiavo@wee.vn>
2023-03-11 18:13:46 +0700

    To The Moon

--- app/src/main/java/com/example/test_teams_notify/FirstFragment.kt
+++ app/src/main/java/com/example/test_teams_notify/FirstFragment.kt
@@ -25,6 +25,7 @@ class FirstFragment : Fragment() {
     ): View? {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
